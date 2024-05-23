3c3abfd4fea33f1c414446d08e93d794b9246ff3
Moham <mohamnotmad@gmail.com>
2024-05-22 20:42:31 -0700

    initial

--- distributed-machine-1/start-ca.sh
+++ distributed-machine-1/start-ca.sh
@@ -6,17 +6,36 @@ mkdir logs
 
 echo
 infoln "Starting ca org1"
-echo
 
 export FABRIC_CA_HOME=./organizations/fabric-ca/org1
 export FABRIC_CA_SERVER_CA_NAME=ca-org1
 export FABRIC_CA_SERVER_TLS_ENABLED=true
-export FABRIC_CA_SERVER_ADDRESS=192.168.0.151
+export FABRIC_CA_SERVER_ADDRESS=192.168.0.154
 export FABRIC_CA_SERVER_PORT=7054
-export FABRIC_CA_SERVER_OPERATIONS_LISTENADDRESS=192.168.0.151:17054
-export FABRIC_CA_SERVER_CSR_HOSTS="localhost,192.168.0.151"
+export FABRIC_CA_SERVER_OPERATIONS_LISTENADDRESS=192.168.0.154:17054
+export FABRIC_CA_SERVER_CSR_HOSTS="localhost,192.168.0.154"
 
 fabric-ca-server start -b admin:adminpw -d > logs/ca-org1.log 2>&1 &
+
+successln "Done..."
+
+
+
+
+
+
+echo
+infoln "Starting ca orderer"
+
+export FABRIC_CA_HOME=./organizations/fabric-ca/ordererOrg
+export FABRIC_CA_SERVER_CA_NAME=ca-orderer
+export FABRIC_CA_SERVER_TLS_ENABLED=true
+export FABRIC_CA_SERVER_ADDRESS=192.168.0.154
+export FABRIC_CA_SERVER_PORT=9054
+export FABRIC_CA_SERVER_OPERATIONS_LISTENADDRESS=192.168.0.154:19054
+export FABRIC_CA_SERVER_CSR_HOSTS="localhost,192.168.0.154"
+
+fabric-ca-server start -b admin:adminpw -d > logs/ca-orderer.log 2>&1 &
 
 successln "Done..."
 
--- distributed-machine-1/start-peer1.sh
+++ distributed-machine-1/start-peer1.sh
@@ -4,46 +4,10 @@ export PATH=${PWD}/../bin:$PATH
 
 . scripts/utils.sh
 
-# infoln "Staring orderer"
 
-# mkdir -p  ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/admincerts
-# cp  ${PWD}/organizations/ordererOrganizations/example.com/users/Admin@example.com/msp/signcerts/cert.pem  ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/admincerts/cert.pem
-
-
-
-
-# export FABRIC_LOGGING_SPEC=INFO
-# export ORDERER_GENERAL_LISTENADDRESS=192.168.0.151
-# export ORDERER_GENERAL_LISTENPORT=7050
-# export ORDERER_GENERAL_LOCALMSPID=OrdererMSP
-# export ORDERER_GENERAL_LOCALMSPDIR=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp
-# # enabled TLS
-# export ORDERER_GENERAL_TLS_ENABLED=true
-# export ORDERER_GENERAL_TLS_PRIVATEKEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
-# export ORDERER_GENERAL_TLS_CERTIFICATE=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt
-# export ORDERER_GENERAL_TLS_ROOTCAS=[${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt]
-# export ORDERER_GENERAL_CLUSTER_CLIENTCERTIFICATE=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt
-# export ORDERER_GENERAL_CLUSTER_CLIENTPRIVATEKEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
-# export ORDERER_GENERAL_CLUSTER_ROOTCAS=[${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt]
-# export ORDERER_GENERAL_BOOTSTRAPMETHOD=none
-# export ORDERER_CHANNELPARTICIPATION_ENABLED=true
-# export ORDERER_ADMIN_TLS_ENABLED=true
-# export ORDERER_ADMIN_TLS_CERTIFICATE=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt
-# export ORDERER_ADMIN_TLS_PRIVATEKEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
-# export ORDERER_ADMIN_TLS_ROOTCAS=[${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt]
-# export ORDERER_ADMIN_TLS_CLIENTROOTCAS=[${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt]
-# export ORDERER_ADMIN_LISTENADDRESS=192.168.0.151:7053
-# export ORDERER_OPERATIONS_LISTENADDRESS=192.168.0.151:9443
-# export ORDERER_METRICS_PROVIDER=prometheus
-
-# orderer > logs/orderer.log 2>&1 &
-
-
-
-
-
-
+echo
 infoln "Starting peer1 org1"
+echo
 
 mkdir ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/msp/admincerts
 cp ${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/cert.pem ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/msp/admincerts/cert.pem
