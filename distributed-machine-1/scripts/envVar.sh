4c7e54bcb1543b81a733be05b89ac73dc704bb09
moamda <mohamnotmad@gmail.com>
2024-05-24 00:10:06 +0000

    removed all crypto materials

--- distributed-machine-1/scripts/envVar.sh
+++ distributed-machine-1/scripts/envVar.sh
@@ -17,38 +17,6 @@ export PEER0_ORG2_CA=${PWD}/organizations/peerOrganizations/org2.example.com/tls
 export PEER0_ORG3_CA=${PWD}/organizations/peerOrganizations/org3.example.com/tlsca/tlsca.org3.example.com-cert.pem
 
 # Set environment variables for the peer org
-# setGlobals() {
-#   local USING_ORG=""
-#   if [ -z "$OVERRIDE_ORG" ]; then
-#     USING_ORG=$1
-#   else
-#     USING_ORG="${OVERRIDE_ORG}"
-#   fi
-#   infoln "Using organization ${USING_ORG}"
-#   if [ $USING_ORG -eq 1 ]; then
-#     export CORE_PEER_LOCALMSPID="Org1MSP"
-#     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
-#     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-#     export CORE_PEER_ADDRESS=192.168.0.151:7051
-#   elif [ $USING_ORG -eq 2 ]; then
-#     export CORE_PEER_LOCALMSPID="Org2MSP"
-#     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
-#     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-#     export CORE_PEER_ADDRESS=192.168.0.151:9051
-#   elif [ $USING_ORG -eq 3 ]; then
-#     export CORE_PEER_LOCALMSPID="Org3MSP"
-#     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG3_CA
-#     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
-#     export CORE_PEER_ADDRESS=192.168.0.151:11051
-#   else
-#     errorln "ORG Unknown"
-#   fi
-
-#   if [ "$VERBOSE" == "true" ]; then
-#     env | grep CORE
-#   fi
-# }
-
 setGlobals() {
   local USING_ORG=""
   if [ -z "$OVERRIDE_ORG" ]; then
@@ -57,22 +25,21 @@ setGlobals() {
     USING_ORG="${OVERRIDE_ORG}"
   fi
   infoln "Using organization ${USING_ORG}"
-
   if [ $USING_ORG -eq 1 ]; then
     export CORE_PEER_LOCALMSPID="Org1MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-
-    if [ $PEER -eq 0 ]; then
-      export CORE_PEER_ADDRESS=192.168.0.151:7051
-    elif [ $PEER -eq 1 ]; then
-      export CORE_PEER_ADDRESS=192.168.0.153:7051
-    else
-      errorln "Peer${PEER} unknown"
-    fi
-
+    export CORE_PEER_ADDRESS=192.168.0.151:7051
   elif [ $USING_ORG -eq 2 ]; then
-    errorln "Org2 is not defined in this network"
+    export CORE_PEER_LOCALMSPID="Org2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    export CORE_PEER_ADDRESS=192.168.0.151:9051
+  elif [ $USING_ORG -eq 3 ]; then
+    export CORE_PEER_LOCALMSPID="Org3MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG3_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
+    export CORE_PEER_ADDRESS=192.168.0.151:11051
   else
     errorln "ORG Unknown"
   fi
