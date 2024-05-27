a2d104ea6353e2376f1180c79d08f1f7789a588f
moamda <mohamnotmad@gmail.com>
2024-05-27 11:33:23 +0000

    asd

--- distributed-machine-org1/start-peer1.sh
+++ distributed-machine-org1/start-peer1.sh
@@ -13,7 +13,8 @@ cp ${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.exam
 
 
 export FABRIC_CA_CLIENT_HOME=${PWD}/organizations/peerOrganizations/org1.example.com/
-export FABRIC_CFG_PATH=${PWD}
+# export FABRIC_CFG_PATH=${PWD}
+export FABRIC_CFG_PATH=$PWD/configtx/peer1
 export FABRIC_LOGGING_SPEC=INFO
 #export FABRIC_LOGGING_SPEC=DEBUG
 export CORE_PEER_TLS_ENABLED=true
