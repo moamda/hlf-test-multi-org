e68ddc0ef8155f54f8793854f2c9edd27387ffd2
moamda <mohamnotmad@gmail.com>
2024-05-27 10:17:15 +0000

    Asd

--- distributed-machine-org1/get-genesis.sh
+++ distributed-machine-org1/get-genesis.sh
@@ -1,3 +1,3 @@
-scp -r vm3@192.168.0.13:/home/vm3/hlf-test-multi-org/distributed-machine-1/channel-artifacts ${PWD}
+scp -r vm3@192.168.0.13:/home/vm3/hlf-test-multi-org/distributed-machine-orderer/channel-artifacts ${PWD}
 mkdir -p ${PWD}/organizations/ordererOrganizations/example.com/
-scp -r vm3@192.168.0.13:/home/vm3/hlf-test-multi-org/distributed-machine-1/organizations/ordererOrganizations/example.com/tlsca ${PWD}/organizations/ordererOrganizations/example.com
+scp -r vm3@192.168.0.13:/home/vm3/hlf-test-multi-org/distributed-machine-orderer/organizations/ordererOrganizations/example.com/tlsca ${PWD}/organizations/ordererOrganizations/example.com
