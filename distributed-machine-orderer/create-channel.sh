4dbd57d3494f29f051674fcf0cef0ad0017b7226
moamda <mohamnotmad@gmail.com>
2024-05-31 01:13:02 +0000

    Asd

--- distributed-machine-orderer/create-channel.sh
+++ distributed-machine-orderer/create-channel.sh
@@ -5,7 +5,7 @@ export PATH=${PWD}/../bin:$PATH
 export CORE_PEER_TLS_ENABLED=true
 export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
 export PEER0_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
-# export PEER1_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
+export PEER1_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
 export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt
 export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
 
@@ -35,7 +35,7 @@ echo $CHANNEL_NAME
 # setGlobals 1 # 1 means which organization is being used.
 
 createGenesisBlock
-createChannel
+# createChannel
 
 # channel sequence
 # 1 - createGenesisBlock, done by orderer org
--- distributed-machine-orderer/get-org1.sh
+++ distributed-machine-orderer/get-org1.sh
@@ -1,7 +1,7 @@
 mkdir -p /home/vm3/hlf-test-multi-org/distributed-machine-orderer/organizations/peerOrganizations/org1.example.com/msp/cacerts
 mkdir -p /home/vm3/hlf-test-multi-org/distributed-machine-orderer/organizations/peerOrganizations/org1.example.com/tlsca
 
-scp -r vm1@192.168.0.151:/home/vm1/hlf-test-multi-org/distributed-machine-org1/organizations/peerOrganizations/org1.example.com/msp /home/vm3/hlf-test-multi-org/distributed-machine-orderer/organizations/peerOrganizations/org1.example.com/
+scp -r vm1@192.168.0.21:/home/vm1/hlf-test-multi-org/distributed-machine-org1/organizations/peerOrganizations/org1.example.com/msp /home/vm3/hlf-test-multi-org/distributed-machine-orderer/organizations/peerOrganizations/org1.example.com/
 
-scp -r vm1@192.168.0.151:/home/vm1/hlf-test-multi-org/distributed-machine-org1/organizations/peerOrganizations/org1.example.com/tlsca /home/vm3/hlf-test-multi-org/distributed-machine-orderer/organizations/peerOrganizations/org1.example.com/
+scp -r vm1@192.168.0.21:/home/vm1/hlf-test-multi-org/distributed-machine-org1/organizations/peerOrganizations/org1.example.com/tlsca /home/vm3/hlf-test-multi-org/distributed-machine-orderer/organizations/peerOrganizations/org1.example.com/
 
