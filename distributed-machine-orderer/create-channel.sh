455657c5c26fe9515363b25b15c2d8a061877709
Moham <mohamnotmad@gmail.com>
2024-06-05 22:52:57 -0700

    asd

--- distributed-machine-orderer/create-channel.sh
+++ distributed-machine-orderer/create-channel.sh
@@ -1,19 +1,16 @@
 export FABRIC_CFG_PATH=$PWD/configtx/
 export PATH=${PWD}/../bin:$PATH
 
-
-export CORE_PEER_TLS_ENABLED=true
-export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
-export PEER0_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
-export PEER1_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
-export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt
-export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
-
+# export CORE_PEER_TLS_ENABLED=true
+# export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
+# export PEER0_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
+# export PEER1_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
+# export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt
+# export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
 
 . scripts/envVar.sh
 . scripts/configUpdate.sh
 . scripts/utils.sh
-
 
 createGenesisBlock() {
     configtxgen -profile ChannelUsingRaft -outputBlock ./channel-artifacts/$CHANNEL_NAME/${CHANNEL_NAME}.block -channelID $CHANNEL_NAME
@@ -21,13 +18,32 @@ createGenesisBlock() {
 }
 
 createChannel() {
-    osnadmin channel join --channelID $CHANNEL_NAME --config-block ./channel-artifacts/$CHANNEL_NAME/${CHANNEL_NAME}.block -o 192.168.0.73:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY" > logs/osnadmin.log 2>&1
+    etGlobalForOrderer0
+    osnadmin channel join --channelID $CHANNEL_NAME --config-block ./channel-artifacts/$CHANNEL_NAME/${CHANNEL_NAME}.block -o 192.168.0.73:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY" >logs/osnadmin.log 2>&1
+    cat ./logs/osnadmin.log
+
+    setGlobalForOrderer1
+    osnadmin channel join --channelID $CHANNEL_NAME --config-block ./channel-artifacts/$CHANNEL_NAME/${CHANNEL_NAME}.block -o 192.168.0.74:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY" >logs/osnadmin.log 2>&1
     cat ./logs/osnadmin.log
 
 }
 
+setGlobalForOrderer0() {
+    export CORE_PEER_TLS_ENABLED=true
+    export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
+    export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt
+    export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
+}
+
+setGlobalForOrderer1() {
+    export CORE_PEER_TLS_ENABLED=true
+    export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
+    export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/server.crt
+    export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/server.key
+}
+
 ORG="1"
-CHANNEL_NAME="channel1" 
+CHANNEL_NAME="channel1"
 echo $CHANNEL_NAME
 
 # setGlobals 1 # 1 means which organization is being used.
@@ -38,15 +54,15 @@ createChannel
 # channel sequence
 # 1 - createGenesisBlock, done by orderer org
 # 2 - createChannel, done by orderer org
-# 3 - joinChannel, done by org1 
+# 3 - joinChannel, done by org1
 # 4 - createAnchorPeerUpdate, done by org1
 # 5 - updateAnchorPeer, done by org1
 
-    # Note !!!
-    # Updating anchor peer is made once only by org1
-    # if you have other peers are not yet join
-    # just set the environment variable of that specific peer of organization
-    # then you can join that peer to the channel.
+# Note !!!
+# Updating anchor peer is made once only by org1
+# if you have other peers are not yet join
+# just set the environment variable of that specific peer of organization
+# then you can join that peer to the channel.
 
 # Set the environtment variable of peer1 to join channel
 # 6 - 'setGlobals 1' or use the function 'setGlobalsForPeer1Org1'
